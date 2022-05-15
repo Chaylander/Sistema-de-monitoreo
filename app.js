25cc462699e30efc2b54daf95262e2d98f40a01b
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-15 18:17:10 -0500

    Elimino botón que muestra los derrames, ahora lo hace de manera automática

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 var map = L.map('map').setView(new L.LatLng(23, -102), 5);
 
-const fetchDrawonMap = () => {
-    const url = 'https://hackoceanmapamb.azurewebsites.net/mapamb/reports';
-    fetch(url).then((res) =>{
-        //res arroja un status de 200, quiere decir que se encontro. Son los errores como el 404 que significa que no se encontro
-        if(res.status !=200){
-            console.log(res);
-            console.log("Failed to fetch data");
-        }
-        else{
-            return res.json();
-        }
-    })
-    .then((data) =>{
-        console.log(data);
-        let NumeroCoordenadas = data.length;
-        console.log("Numero de coordenadas: " + NumeroCoordenadas);
+const url = 'https://hackoceanmapamb.azurewebsites.net/mapamb/reports';
+fetch(url).then((res) =>{
+    //res arroja un status de 200, quiere decir que se encontro. Son los errores como el 404 que significa que no se encontro
+    if(res.status !=200){
+        console.log(res);
+        console.log("Failed to fetch data");
+    }
+    else{
+        return res.json();
+    }
+})
+.then((data) =>{
+    console.log(data);
+    let NumeroCoordenadas = data.length;
+    console.log("Numero de coordenadas: " + NumeroCoordenadas);
 
-        for(let i=0; i<=NumeroCoordenadas; i++){
-            let coordenadas = data[i].lugar;
-            let coordenadasSplit = coordenadas.split(",");
-            console.log(coordenadasSplit);
-            let marker = L.circleMarker(coordenadasSplit, {
-                color: 'red',
-                fillColor: 'red',
-                fillOpacity: 0.5,
-                radius: 20
-            }).addTo(map);
-            marker.bindPopup("<b>Coordenadas: </b>" + coordenadas + "\n <b>Contaminante: </b>" + data[i].contaminante + "\n <b>Fecha: </b>" + data[i].fecha);
-        }
+    for(let i=0; i<=NumeroCoordenadas; i++){
+        let coordenadas = data[i].lugar;
+        let coordenadasSplit = coordenadas.split(",");
+        console.log(coordenadasSplit);
+        let marker = L.circleMarker(coordenadasSplit, {
+            color: 'red',
+            fillColor: 'red',
+            fillOpacity: 0.5,
+            radius: 20
+        }).addTo(map);
+        marker.bindPopup("<b>Coordenadas: </b>" + coordenadas + "\n <b>Contaminante: </b>" + data[i].contaminante + "\n <b>Fecha: </b>" + data[i].fecha);
+    }
 });
-}
+
 
 
 
