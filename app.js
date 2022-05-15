a0089918c1c15069fe706e587c2ce4046912ee00
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-15 13:10:46 -0500

    Fix para hacer consultas a la API y dibujar las ubicaciones en el mapa(circulo con fecha, contaminante y coordenadas), cambio al boton. Acomodo del mapa

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-
 var map = L.map('map').setView(new L.LatLng(23, -102), 5);
+
+const fetchDrawonMap = () => {
+    const url = 'https://hackoceanmapamb.azurewebsites.net/mapamb/reports';
+    fetch(url).then((res) =>{
+        //res arroja un status de 200, quiere decir que se encontro. Son los errores como el 404 que significa que no se encontro
+        if(res.status !=200){
+            console.log(res);
+            console.log("Failed to fetch data");
+        }
+        else{
+            return res.json();
+        }
+    })
+    .then((data) =>{
+        console.log(data);
+        let NumeroCoordenadas = data.length;
+        console.log("Numero de coordenadas: " + NumeroCoordenadas);
+        for(let i=01; i<4; i++){
+            let coordenadas = data[i].lugar;
+            let coordenadasSplit = coordenadas.split(",");
+            console.log(coordenadasSplit);
+            let marker = L.circleMarker(coordenadasSplit).addTo(map);
+            marker.bindPopup("<b>Coordenadas: </b>" + coordenadas + "\n <b>Contaminante: </b>" + data[i].contaminante + "\n <b>Fecha: </b>" + data[i].fecha);
+        }
+});
+}
+
+
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -11,6 +39,8 @@ var circle = L.circle([23, -102], {
     fillOpacity: 0.5,
     radius: 500
 }).addTo(map);
+
+
 
 
 function onMapClick(e) {
